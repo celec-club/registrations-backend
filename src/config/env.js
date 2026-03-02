8e1c2b1f5aeb25405b4f91800f7931aeace652ae
Yasser <yasserfedsi34@gmail.com>
2026-03-02 23:47:55 +0100

    refactor(env): restructure dotenv configuration and enhance DATABASE_URL handling

--- src/config/env.js
+++ src/config/env.js
@@ -1,5 +1,14 @@
-require("dotenv").config();
+const path = require("path");
+const dotenv = require("dotenv");
+
+dotenv.config({
+  path: path.resolve(process.cwd(), ".env"),
+});
 
 module.exports = {
   PORT: Number(process.env.PORT) || 9800,
+  DATABASE_URL:
+    process.env.NODE_ENV === "production"
+      ? process.env.DATABASE_URL_DEPLOY
+      : process.env.DATABASE_URL,
 };
